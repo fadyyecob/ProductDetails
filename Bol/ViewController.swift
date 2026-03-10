7e029d2c7d0613ae335fb4982984ef29235101e7
Fady Yecob <fady.yecob@justeattakeaway.com>
2026-03-10 21:15:45 +0100

    Initial Commit

---
+++ Bol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bol
+//
+//  Created by Fady Yecob on 10/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BolTests/BolTests.swift
@@ -0,0 +1,17 @@
+//
+//  BolTests.swift
+//  BolTests
+//
+//  Created by Fady Yecob on 10/03/2026.
+//
+
+import Testing
+@testable import Bol
+
+struct BolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
